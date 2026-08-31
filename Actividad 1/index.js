9770746bfefede38e744256d804800c9bbce22c5
sofiachimento-blip <sofiachimentogmail.com>
2026-08-31 15:01:38 -0300

    Actividad 1

---
+++ Actividad 1/index.js
@@ -0,0 +1,8 @@
+async function obtenerDatos(){
+    // Response
+    const respuesta = await fetch('./datos/productos.json')
+    const productos = await respuesta.json() // <-- javascript
+    productos.forEach(()=>{
+        
+    })
+}
